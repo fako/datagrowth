10147bfad5a227e28b577f9dffdfb052bfecb425
Fako Berkers <email@fakoberkers.nl>
2019-09-03 20:30:08 +0200

    Adds code to setup the package

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-.. Data Growth documentation master file, created by
-   sphinx-quickstart on Wed Dec 17 18:26:35 2014.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Data Growth's documentation
-===========================
+DATAGROWTH
+==========
 
 Data Growth is the bridge between gathered data and algorithms.
 The demand for data is increasing, because many algorithms and computer models need more of it than ever before.
@@ -16,21 +11,3 @@ for the data gathering and preprocessing as well as pipelines for filtering and 
 
 The package offers classes that help you load the data into your models.
 Or you can start the Django server to transfer data over a REST API to other devices or services.
-
-Contents
---------
-
-.. toctree::
-   :maxdepth: 2
-
-   resources/index
-   configuration/index
-   reference
-
-
-Indices and tables
-------------------
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+VERSION = "0.15.0"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datagrowth",
+    version=VERSION,
+    author="Fako Berkers",
+    author_email="email@fakoberkers.nl",
+    description="A data mash up framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fako/datagrowth",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "Django>=1.11",
+        "jsonschema",
+        "beautifulsoup4",
+        "urlobject",
+        "requests",
+        "psycopg2-binary",
+        "Pillow>=5",
+        "tqdm",
+        "django-json-field",
+    ],
+    python_requires="~=3.5",
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+)
