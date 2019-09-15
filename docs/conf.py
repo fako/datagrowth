646a58c7e639e8938a24e1e32be389702279f839
Fako Berkers <email@fakoberkers.nl>
2019-09-15 15:18:01 +0200

    Fixes documentation creation

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ import sphinx_rtd_theme
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(os.path.join('..', 'datagrowth')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'tests')))
 sys.path.insert(0, os.path.abspath(os.path.join('..')))
 
 # setup django settings
