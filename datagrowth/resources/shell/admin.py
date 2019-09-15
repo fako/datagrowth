2d6a69fefb4a4aa4fa054b457407d63bf0300e68
Fako Berkers <email@fakoberkers.nl>
2019-09-15 10:02:08 +0200

    Adds request and command fields to search fields for resources in admin

--- datagrowth/resources/shell/admin.py
+++ datagrowth/resources/shell/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 
 class ShellResourceAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'uri', 'status', 'stderr', 'config', 'created_at', 'modified_at']
-    search_fields = ['uri', 'stdout', 'stderr']
+    search_fields = ['uri', 'stdout', 'stderr', 'command']
