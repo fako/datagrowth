92b6ee3064981f1b374a511fb6f69c4f79f4c7f7
Fako Berkers <email@fakoberkers.nl>
2019-09-16 10:29:28 +0200

    Adds assertions for small lists and ibatch

--- tests/utils/tests/iterators.py
+++ tests/utils/tests/iterators.py
@@ -24,6 +24,20 @@ class TestIBatch(TestCase):
         self.assertEqual(len(batches), 10)
         self.assertEqual(batches[0], list(range(0, 11)))
         self.assertEqual(batches[-1], list(range(99, 100)))
+        # Batch size equals list length
+        input = list(range(0, 10))
+        iterator = ibatch(input, batch_size=10)
+        self.assertIsInstance(iterator, Iterator)
+        batches = [batch for batch in iterator]
+        self.assertEqual(len(batches), 1)
+        self.assertEqual(batches[0], list(range(0, 10)))
+        # Batch size larger than list length
+        input = list(range(0, 11))
+        iterator = ibatch(input, batch_size=20)
+        self.assertIsInstance(iterator, Iterator)
+        batches = [batch for batch in iterator]
+        self.assertEqual(len(batches), 1)
+        self.assertEqual(batches[0], list(range(0, 11)))
 
     def test_ibatch_with_iterator_no_progress(self):
         input = range(0, 100)
@@ -41,6 +55,20 @@ class TestIBatch(TestCase):
         self.assertEqual(len(batches), 10)
         self.assertEqual(batches[0], list(range(0, 11)))
         self.assertEqual(batches[-1], list(range(99, 100)))
+        # Batch size equals list length
+        input = range(0, 10)
+        iterator = ibatch(input, batch_size=10)
+        self.assertIsInstance(iterator, Iterator)
+        batches = [batch for batch in iterator]
+        self.assertEqual(len(batches), 1)
+        self.assertEqual(batches[0], list(range(0, 10)))
+        # Batch size larger than list length
+        input = range(0, 11)
+        iterator = ibatch(input, batch_size=20)
+        self.assertIsInstance(iterator, Iterator)
+        batches = [batch for batch in iterator]
+        self.assertEqual(len(batches), 1)
+        self.assertEqual(batches[0], list(range(0, 11)))
 
     def test_ibatch_with_list_progress_no_total(self):
         progress_bar_mock = Mock(spec=tqdm(disable=True))
@@ -68,6 +96,30 @@ class TestIBatch(TestCase):
             tqdm_mock.assert_called_once_with()
             self.assertEqual(progress_bar_mock.update.call_count, 10)
             self.assertEqual(progress_bar_mock.close.call_count, 1)
+        progress_bar_mock.reset_mock()
+        input = range(0, 10)
+        with patch("datagrowth.utils.iterators.tqdm", return_value=progress_bar_mock) as tqdm_mock:
+            # Batch size equals list length
+            iterator = ibatch(input, batch_size=10, progress_bar=True)
+            self.assertIsInstance(iterator, Iterator)
+            batches = [batch for batch in iterator]
+            self.assertEqual(len(batches), 1)
+            self.assertEqual(batches[0], list(range(0, 10)))
+            tqdm_mock.assert_called_once_with()
+            self.assertEqual(progress_bar_mock.update.call_count, 1)
+            self.assertEqual(progress_bar_mock.close.call_count, 1)
+        progress_bar_mock.reset_mock()
+        input = range(0, 11)
+        with patch("datagrowth.utils.iterators.tqdm", return_value=progress_bar_mock) as tqdm_mock:
+            # Batch size larger than list length
+            iterator = ibatch(input, batch_size=20, progress_bar=True)
+            self.assertIsInstance(iterator, Iterator)
+            batches = [batch for batch in iterator]
+            self.assertEqual(len(batches), 1)
+            self.assertEqual(batches[0], list(range(0, 11)))
+            tqdm_mock.assert_called_once_with()
+            self.assertEqual(progress_bar_mock.update.call_count, 1)
+            self.assertEqual(progress_bar_mock.close.call_count, 1)
 
     def test_ibatch_with_list_progress_and_total(self):
         input = range(0, 100)
@@ -94,4 +146,28 @@ class TestIBatch(TestCase):
             self.assertEqual(batches[-1], list(range(99, 100)))
             tqdm_mock.assert_called_once_with(total=10)
             self.assertEqual(progress_bar_mock.update.call_count, 10)
+            self.assertEqual(progress_bar_mock.close.call_count, 1)
+            progress_bar_mock.reset_mock()
+        input = range(0, 10)
+        with patch("datagrowth.utils.iterators.tqdm", return_value=progress_bar_mock) as tqdm_mock:
+            # Batch size equals list length
+            iterator = ibatch(input, batch_size=10, progress_bar=True, total=10)
+            self.assertIsInstance(iterator, Iterator)
+            batches = [batch for batch in iterator]
+            self.assertEqual(len(batches), 1)
+            self.assertEqual(batches[0], list(range(0, 10)))
+            tqdm_mock.assert_called_once_with(total=1)
+            self.assertEqual(progress_bar_mock.update.call_count, 1)
+            self.assertEqual(progress_bar_mock.close.call_count, 1)
+        progress_bar_mock.reset_mock()
+        input = range(0, 11)
+        with patch("datagrowth.utils.iterators.tqdm", return_value=progress_bar_mock) as tqdm_mock:
+            # Batch size larger than list length
+            iterator = ibatch(input, batch_size=20, progress_bar=True, total=11)
+            self.assertIsInstance(iterator, Iterator)
+            batches = [batch for batch in iterator]
+            self.assertEqual(len(batches), 1)
+            self.assertEqual(batches[0], list(range(0, 11)))
+            tqdm_mock.assert_called_once_with(total=1)
+            self.assertEqual(progress_bar_mock.update.call_count, 1)
             self.assertEqual(progress_bar_mock.close.call_count, 1)
