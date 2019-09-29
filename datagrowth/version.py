74bb796d040625ed97b9d278b2ea69de661ab332
Fako Berkers <email@fakoberkers.nl>
2019-09-29 17:38:45 +0200

    Builds docs in a way to work with deploy repo

--- datagrowth/version.py
+++ datagrowth/version.py
@@ -1,1 +1,9 @@
+import json
+
+
 VERSION = "0.15.0"
+
+
+def create_package_json(file_path):
+    with open(file_path, "w") as json_file:
+        json.dump({"name": "datagrowth", "version": VERSION}, json_file, indent=4)
