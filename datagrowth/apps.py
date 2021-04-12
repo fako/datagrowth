f739d03a7f9bc2f0c991c338c8ff945d8629ad9d
Fako Berkers <email@fakoberkers.nl>
2021-04-12 15:09:30 +0200

    Makes Processor available in datagrowth.processors

--- datagrowth/apps.py
+++ datagrowth/apps.py
@@ -15,14 +15,17 @@ class DatagrowthConfig(AppConfig):
     def load_processors(self):
         from datagrowth.processors.base import Processor
         self.processors = {}
+        datagrowth_processors = set()
         for app_config in apps.get_app_configs():
             try:
                 processor_module = importlib.import_module(app_config.module.__name__ + ".processors")
                 for name, attr in processor_module.__dict__.items():
-                    if name in self.processors:
+                    if name in self.processors and name not in datagrowth_processors:
                         raise RuntimeError("The {} Processor is being loaded twice".format(name))
                     if inspect.isclass(attr) and issubclass(attr, Processor):
                         self.processors[name] = attr
+                    if app_config.name == "datagrowth":
+                        datagrowth_processors.add(name)
             except ImportError:
                 continue
 
