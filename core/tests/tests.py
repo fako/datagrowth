c614c07f32e2d535c5084e1c5e804edf37df93cb
Fako Berkers <email@fakoberkers.nl>
2021-05-10 17:24:40 +0200

    Removes many ported tests from the core reference app

--- core/tests/tests.py
+++ core/tests/tests.py
@@ -7,19 +7,8 @@ from core.processors.tests.resources import TestHttpResourceProcessor
 from core.processors.tests.rank import TestRankProcessor, TestRankProcessorLegacy
 from core.processors.tests.expansion import TestExpansionProcessor
 from core.processors.tests.compare import TestCompareProcessor
-from core.processors.tests.manifest import TestManifestProcessor
 
 from core.models.organisms.tests.growth import TestGrowth
-from core.models.organisms.tests.community import TestCommunityMock
 from core.models.organisms.managers.tests.community import TestCommunityManager
-from core.models.organisms.tests.collective import TestCollective
-from core.models.organisms.tests.individual import TestIndividual
-from core.models.resources.tests.manifestation import TestManifestationResource
 
-from core.tasks.tests.http import (TestSendMassTaskGet, TestSendMassTaskPost, TestSendTaskGet, TestSendTaskPost,
-                                   TestSendSerieTaskGet, TestSendSerieTaskPost, TestGetResourceLink, TestLoadSession)
-from core.tasks.tests.manifestation import TestManifestationTasks
-
-from core.views.tests.collective import TestCollectiveView, TestCollectiveContentView
-from core.views.tests.individual import TestIndividualView, TestIndividualContentView
 from core.views.tests.community import TestCommunityView, TestHtmlCommunityView
