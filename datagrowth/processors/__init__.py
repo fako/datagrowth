c1518a5cd5e4781ccc24390080320760a490228c
Fako Berkers <email@fakoberkers.nl>
2023-10-13 19:12:01 +0200

    Adds a first version of the HttpSeedingProcessor

--- datagrowth/processors/__init__.py
+++ datagrowth/processors/__init__.py
@@ -1,4 +1,4 @@
 from .base import Processor, QuerySetProcessor, ProcessorFactory
 from .growth import GrowthProcessor
-from .input import ExtractProcessor
+from .input import ExtractProcessor, HttpSeedingProcessor
 from .resources.growth import HttpGrowthProcessor
--- tests/processors/tests/__init__.py
+++ tests/processors/tests/__init__.py
@@ -2,3 +2,4 @@ from .base import TestProcessorBase, TestDatagrowthProcessorDjangoConfig
 from .extraction import TestExtractProcessor
 from .iterators import TestContentIteratorWithSendIterator, TestContentIteratorWithSendSerieIterator
 from .growth.http import TestHttpGrowthProcessor
+from .seeding.simple import TestSimpleHttpSeedingProcessor, TestSimpleDeltaHttpSeedingProcessor
