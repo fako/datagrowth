94983db14d7b03fe41f2921deea19364da6a1b81
Fako Berkers <email@fakoberkers.nl>
2023-11-05 10:58:20 +0100

    Implements tests for HttpGrowthProcessor

--- tests/processors/tests/growth/http.py
+++ tests/processors/tests/growth/http.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.test import TestCase
+from django.contrib.contenttypes.models import ContentType
 
 from datagrowth.configuration import register_defaults
 from datagrowth.processors import HttpGrowthProcessor
@@ -32,6 +33,15 @@ class TestHttpGrowthProcessor(TestCase):
         super().setUp()
         self.collection = Collection.objects.get(id=3)
 
+    def assert_batch_and_process_results(self, batch_count=2):
+        self.assertEqual(Batch.objects.count(), batch_count)
+        for batch in Batch.objects.all():
+            self.assertEqual(batch.documents.count(), 0, "Expected all batch instances to have zero documents")
+        self.assertEqual(
+            ProcessResult.objects.count(), 0,
+            "Expected all ProcessResults to be cleared after successful processing to release Document locks"
+        )
+
     def test_synchronous_update(self):
         processor = HttpGrowthProcessor({
             "growth_phase": "test",
@@ -55,13 +65,160 @@ class TestHttpGrowthProcessor(TestCase):
         self.assertEqual(self.collection.documents.count(), 3)
         for ix, document in enumerate(self.collection.documents.all()):
             self.assertEqual(document.derivatives, {"test": {"extra": f"test {ix}"}})
-        self.assertEqual(Batch.objects.count(), 2)
-        for batch in Batch.objects.all():
-            self.assertEqual(batch.documents.count(), 0, "Expected all batch instances to have zero documents")
-        self.assertEqual(
-            ProcessResult.objects.count(), 0,
-            "Expected all ProcessResults to be cleared after successful processing to release Document locks"
-        )
+        self.assert_batch_and_process_results()
+
+    def test_synchronous_to_property(self):
+        processor = HttpGrowthProcessor({
+            "growth_phase": "test",
+            "datatypes_app_label": "datatypes",
+            "batch_size": 2,
+            "asynchronous": False,
+            "to_property": "properties/results",
+            "retrieve_data": {
+                "resource": "resources.httpresourcemock",
+                "method": "get",
+                "args": ["$.resource"],
+                "kwargs": {},
+            },
+            "contribute_data": {
+                "objective": {
+                    "@": "$.0",
+                    "extra": "$.extra"
+                }
+            }
+        })
+        processor(self.collection.documents)
+        self.assertEqual(self.collection.documents.count(), 3)
+        for ix, document in enumerate(self.collection.documents.all()):
+            self.assertEqual(document.properties["results"], {"extra": f"test {ix}"})
+        self.assert_batch_and_process_results()
+
+    def test_synchronous_depends_on(self):
+        # Setting the dependency on documents
+        success, failure, unknown = list(self.collection.documents.all())
+        success.task_results = {"dependency": {"success": True}}
+        success.save()
+        failure.task_results = {"dependency": {"success": False}}
+        failure.save()
+        # Calling processor with depends_on set
+        processor = HttpGrowthProcessor({
+            "growth_phase": "test",
+            "datatypes_app_label": "datatypes",
+            "batch_size": 2,
+            "asynchronous": False,
+            "depends_on": "dependency",
+            "retrieve_data": {
+                "resource": "resources.httpresourcemock",
+                "method": "get",
+                "args": ["$.resource"],
+                "kwargs": {},
+            },
+            "contribute_data": {
+                "objective": {
+                    "@": "$.0",
+                    "extra": "$.extra"
+                }
+            }
+        })
+        processor(self.collection.documents)
+        # Asserting results
+        self.assertEqual(self.collection.documents.count(), 3)
+        for ix, document in enumerate(self.collection.documents.all()):
+            if document.id == success.id:
+                self.assertEqual(document.derivatives, {"test": {"extra": f"test {ix}"}})
+            else:
+                self.assertEqual(document.derivatives, {})
+        self.assert_batch_and_process_results(batch_count=1)
+
+    def test_synchronous_apply_resource_to(self):
+        processor = HttpGrowthProcessor({
+            "growth_phase": "test",
+            "datatypes_app_label": "datatypes",
+            "batch_size": 2,
+            "asynchronous": False,
+            "apply_resource_to": ["reference"],
+            "retrieve_data": {
+                "resource": "resources.httpresourcemock",
+                "method": "get",
+                "args": ["$.resource"],
+                "kwargs": {},
+            },
+            "contribute_data": {
+                "objective": {
+                    "@": "$.0",
+                    "extra": "$.extra"
+                }
+            }
+        })
+        processor(self.collection.documents)
+        self.assertEqual(self.collection.documents.count(), 3)
+        for ix, document in enumerate(self.collection.documents.all()):
+            self.assertEqual(document.reference, "200", "Expected Documents to take on Resource status as a reference")
+
+    def test_synchronous_document_lock(self):
+        # Setting a lock on a single document
+        result_type = ContentType.objects.get_by_natural_key("resources", "httpresourcemock")
+        locked, free, _ = list(self.collection.documents.all())
+        batch = Batch.objects.create()
+        locked_process_result = ProcessResult.objects.create(batch=batch, document=locked, result_type=result_type)
+        free_process_result = ProcessResult.objects.create(batch=batch, document=free)
+        # Calling processor like normal
+        processor = HttpGrowthProcessor({
+            "growth_phase": "test",
+            "datatypes_app_label": "datatypes",
+            "batch_size": 2,
+            "asynchronous": False,
+            "retrieve_data": {
+                "resource": "resources.httpresourcemock",
+                "method": "get",
+                "args": ["$.resource"],
+                "kwargs": {},
+            },
+            "contribute_data": {
+                "objective": {
+                    "@": "$.0",
+                    "extra": "$.extra"
+                }
+            }
+        })
+        processor(self.collection.documents)
+        # Release the test locks to assert Batch and ProcessResult
+        locked_process_result.delete()
+        free_process_result.delete()
+        # Asserting results
+        self.assertEqual(self.collection.documents.count(), 3)
+        for ix, document in enumerate(self.collection.documents.all()):
+            if document.id == locked.id:
+                self.assertEqual(document.derivatives, {})
+            else:
+                self.assertEqual(document.derivatives, {"test": {"extra": f"test {ix}"}})
+        self.assert_batch_and_process_results()
+
+    def test_synchronous_pass_resource_through(self):
+        processor = HttpGrowthProcessor({
+            "growth_phase": "test",
+            "datatypes_app_label": "datatypes",
+            "batch_size": 2,
+            "asynchronous": False,
+            "extractor": "ExtractProcessor.pass_resource_through",
+            "retrieve_data": {
+                "resource": "resources.httpresourcemock",
+                "method": "get",
+                "args": ["$.resource"],
+                "kwargs": {},
+            },
+            "contribute_data": {
+                "objective": {
+                    "@": "$.0",
+                    "extra": "$.extra"
+                }
+            }
+        })
+        processor(self.collection.documents)
+        self.assertEqual(self.collection.documents.count(), 3)
+        for ix, document in enumerate(self.collection.documents.all()):
+            self.assertEqual(document.derivatives, {"test": {"extra": f"test {ix}", "value": f"nested value {ix}"}})
+        self.assert_batch_and_process_results()
 
     def test_asynchronous_update(self):
         self.skipTest("Redis should get enabled")
