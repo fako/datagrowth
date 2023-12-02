08332d9f68f12a8d3c1ac228d9273441a4a50793
Fako Berkers <email@fakoberkers.nl>
2023-12-02 20:54:56 +0100

    Modernizes mock processor interface

--- tests/processors/processors/output.py
+++ tests/processors/processors/output.py
@@ -9,20 +9,20 @@ class MockProcessor(Processor):
 
 class MockNumberProcessor(MockProcessor):
 
-    def number_individuals(self, individuals):
+    def number_documents(self, documents):
         def number_individual(individual, number):
             individual["number"] = number
             return individual
-        return (number_individual(individual, idx+1) for idx, individual in enumerate(individuals))
+        return (number_individual(individual, idx+1) for idx, individual in enumerate(documents))
 
 
 class MockFilterProcessor(MockProcessor):
 
-    def filter_individuals(self, individuals):
-        for individual in individuals:
-            if self.config.include_odd and individual.get("number") % 2:
-                yield individual
-            elif self.config.include_even and not individual.get("number") % 2:
-                yield individual
+    def filter_documents(self, documents):
+        for document in documents:
+            if self.config.include_odd and document.get("number") % 2:
+                yield document
+            elif self.config.include_even and not document.get("number") % 2:
+                yield document
             elif self.config.include_odd and self.config.include_even:
-                yield individual
+                yield document
