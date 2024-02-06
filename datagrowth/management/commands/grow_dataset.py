71579bb86e8296731418fd65bf0257f10b362a65
Fako Berkers <email@fakoberkers.nl>
2024-02-06 14:26:59 +0100

    Lets grow_dataset wait for completion

--- datagrowth/management/commands/grow_dataset.py
+++ datagrowth/management/commands/grow_dataset.py
@@ -1,4 +1,5 @@
 import logging
+from time import sleep
 
 from datagrowth.management.base import DatasetCommand
 from datagrowth.datatypes.datasets.constants import GrowthStrategy
@@ -19,6 +20,8 @@ class Command(DatasetCommand):
         parser.add_argument('-l', '--limit', type=int, nargs="?")
         parser.add_argument('-r', '--retry', action="store_true")
         parser.add_argument('-i', '--initial-seeder', type=str, nargs="?")
+        parser.add_argument('-t', '--timeout', type=int, default=60*60*24)
+        parser.add_argument('-w', '--wait-interval', type=int, default=10)
 
     def get_datasets(self):
         raise TypeError("It is impossible to grow multiple datasets at the same time.")
@@ -29,10 +32,24 @@ class Command(DatasetCommand):
         limit = options["limit"]
         retry = options["retry"]
         seeds = options["initial_seeder"]
-        dataset.grow(
+        timeout = options["timeout"]
+        wait_interval = options["wait_interval"]
+
+        group_result = dataset.grow(
             *args,
             growth_strategy=growth_strategy, asynchronous=asynchronous, limit=limit, retry=retry, seeds=seeds
         )
+
+        ready = not asynchronous or not group_result
+        timer = 0
+        while not ready and not timer >= timeout:
+            ready = group_result.ready()
+            sleep(wait_interval)
+            timer += wait_interval
+        else:
+            if timer >= timeout:
+                message = "Grow dataset command exceeded timeout"
+                log.error(message)
 
     ###################################
     # Legacy Community compatability
