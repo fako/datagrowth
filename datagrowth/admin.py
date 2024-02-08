830b903f5409c8f31325f56701b54c7808d7b16c
Fako Berkers <email@fakoberkers.nl>
2024-02-08 17:50:18 +0100

    Adds admin models for new datatypes

--- datagrowth/admin.py
+++ datagrowth/admin.py
@@ -1,4 +1,6 @@
 from datagrowth.resources.http.admin import HttpResourceAdmin
 from datagrowth.resources.shell.admin import ShellResourceAdmin
 from datagrowth.datatypes.annotations.admin import AnnotationAdmin
-from datagrowth.datatypes.documents.admin import DataStorageAdmin, DocumentAdmin
+from datagrowth.datatypes.datasets.admin import DatasetAdmin
+from datagrowth.datatypes.documents.admin import (DocumentAdmin, DataStorageAdmin, TaskDataStorageAdmin,
+                                                  DatasetVersionAdmin, CollectionAdmin, TaskDocumentAdmin)
