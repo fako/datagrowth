388c6169d7631c8579ed2b7d8da50cc27df3b65a
Fako Berkers <email@fakoberkers.nl>
2025-04-24 10:45:13 +0200

    Switches to a modern package installation method

--- commands/utils.py
+++ commands/utils.py
@@ -4,6 +4,6 @@ from invoke import Exit
 
 def assert_repo_root_directory():
     root_directory = os.getcwd()
-    setup_file = os.path.join(root_directory, "setup.py")
-    if not os.path.exists(setup_file):  # apparently we're not executing from root directory
+    project_file = os.path.join(root_directory, "pyproject.toml")
+    if not os.path.exists(project_file):  # apparently we're not executing from root directory
         raise Exit("Command should be run from repository root")
