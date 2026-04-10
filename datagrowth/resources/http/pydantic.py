dc266173b8c02a587acf5f453172dd6cd22751fd
Fako Berkers <email@fakoberkers.nl>
2026-04-10 22:13:57 +0200

    Correct Resource.success return type

--- datagrowth/resources/http/pydantic.py
+++ datagrowth/resources/http/pydantic.py
@@ -218,7 +218,7 @@ class HttpResource(Resource[HttpSignature]):
         )
 
     @property
-    def success(self) -> str:
+    def success(self) -> bool:
         """
         Returns True if status is within HTTP success range
         """
