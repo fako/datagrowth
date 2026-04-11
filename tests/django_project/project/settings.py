3a380b185b20959157681e103d13198562f7c7a1
Fako Berkers <email@fakoberkers.nl>
2026-04-11 20:40:49 +0200

    Move utils tests to proper locations

--- tests/django_project/project/settings.py
+++ tests/django_project/project/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 
     'project',
     'configuration',
-    'utils',
     'resources',
     'processors',
     'datatypes',
--- tests/django_project/utils/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from .data import TestPythonReach, TestOverrideDict, TestIsJSONMimetype
-from .datetime import TestDatetimeUtils
-from .io import TestInputOutputPaths, TestDumpLoadDjangoModels
-from .iterators import TestIBatch
