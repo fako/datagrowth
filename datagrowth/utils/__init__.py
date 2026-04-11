424e9c9d0eeefda12f3e8383010a33b1391a4ee8
Fako Berkers <email@fakoberkers.nl>
2026-04-11 20:40:49 +0200

    Move utils tests to proper locations

--- datagrowth/utils/__init__.py
+++ datagrowth/utils/__init__.py
@@ -3,7 +3,14 @@ from datagrowth.utils.datetime import parse_datetime_string, format_datetime
 from datagrowth.utils.io import (get_model_path, get_media_path, get_dumps_path, object_to_disk, queryset_to_disk,
                                  objects_from_disk)
 from datagrowth.utils.data import reach, override_dict, is_json_mimetype
-from datagrowth.utils.tasks import DatabaseConnectionResetTask
+
+# Django dependant imports that have not yet been untangled
+try:
+    from datagrowth.utils.io import (get_model_path, get_media_path, get_dumps_path, object_to_disk, queryset_to_disk,
+                                     objects_from_disk)
+    from datagrowth.utils.tasks import DatabaseConnectionResetTask
+except ImportError:
+    pass
 
 
 def is_hashable(obj: object) -> bool:
